f5bc41b8dcdc6474d8ed057e4bf0873a58770d6e
Zarritas <132434631+Zarritas@users.noreply.github.com>
2023-09-26 12:08:49 +0000

    Inicio ejecicios basicos desarrollo entorno cliente en js

---
+++ Bloque 4/Desarrollo web en entorno cliente/Practica/buclesJS1.js
@@ -0,0 +1,51 @@
+//Introductorios
+
+function mensajeDomingo(){
+    let dia=prompt("Que dia de la semana es?","introduce el día")
+    if (dia.toLowerCase()=="domingo")
+        window("Ole los caracoleeeee");
+}
+
+function analisisEdad(){
+
+}
+
+function analisisPrecisoEdad(){
+
+}
+
+function mostrarNavegador(){
+
+}
+
+function mostrarMensajePersonalizado(){
+
+}
+
+function impares(){
+
+}
+
+function par(){
+
+}
+
+function potencias(){
+
+}
+
+function contraseña(){
+
+}
+
+function validarContraseña(){
+
+}
+
+function intentosContraseña(){
+
+}
+
+function paresMenosMulti3(){
+
+}
---
+++ Bloque 4/Desarrollo web en entorno cliente/Practica/sintaxisJS1.js
@@ -0,0 +1,26 @@
+//Redes informáticas
+
+function calcSubRedes(){
+    
+}
+
+function validarRed(){
+
+}
+
+function mostrarMascaraRed(){
+
+}
+
+//Sistemas operativos
+function validarOrden(){
+
+}
+
+function mostrarAyuda(){
+
+}
+
+function validarSintaxis(){
+    
+}
