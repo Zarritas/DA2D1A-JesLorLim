f31843fc7cd2cb5208759eaaff4a9d1805f851f7
Jesus Lorenzo Limon <jlorenzolimon@gmail.com>
2023-10-27 11:33:49 +0200

    psp vectores3

--- Bloque 3/Programacion de servicios y procesos/Practica/Interbloqueo/src/Vector.java
+++ Bloque 3/Programacion de servicios y procesos/Practica/Interbloqueo/src/Vector.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Vector {
+import java.util.Arrays;
+
+public class Vector {
+
+    private int[] R1;
+    private int indice1;
+
+    public Vector(int longitud) {
+        this.R1 = new int[longitud];
+        this.indice1 = 0;
+    }
+
+    public void agregarValor(int x){
+        R1[indice1] = x;
+        indice1++;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "R1=" + Arrays.toString(R1) +
+                ", indice1=" + indice1 +
+                '}';
+    }
 }
