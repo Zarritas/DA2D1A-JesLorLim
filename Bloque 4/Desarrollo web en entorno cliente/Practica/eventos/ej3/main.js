e9c945823fa2586e420f29b5172b65f4625d9ef1
Jesus Lorenzo <jesus@iesclaradelrey.es>
2023-10-31 14:06:08 +0100

    cosas de cliente js

--- Bloque 4/Desarrollo web en entorno cliente/Practica/eventos/ej3/main.js
+++ Bloque 4/Desarrollo web en entorno cliente/Practica/eventos/ej3/main.js
@@ -1,14 +1,51 @@
-const headForm = document.getElementById("div_form");
-const headCrono = document.getElementById("div_semaforo");
-const headSemaf = document.getElementById("div_cronometro");
+const INPUT_FORM = document.getElementById("form")
+const FORMULARIO = document.getElementById("formulario");
+const SEMAFORO = document.getElementById("semaforo");
+const CRONOMETRO = document.getElementById("cronometro");
+const HEAD_FORMULARIO = document.getElementById("div_form");
+const HEAD_SEMAFORO = document.getElementById("div_semaforo");
+const HEAD_CRONOMETRO = document.getElementById("div_cronometro");
 
-if (document.querySelector('input[name="opciones"]')) {
-    document.querySelectorAll('input[name="opciones"]').forEach((elem) => {
-      elem.addEventListener("change", function(event) {
-        let item=event.target.value;
-        console.log(item);
-        elem.style.backgroundColor = "red";
-      });
-    });
-  }
+const head = document.getElementsByClassName("head")
+
+
+const checkboxes = document.getElementsByName("videojuegos")
+function verFormulario() {
+    CRONOMETRO.style.display = "none";
+    SEMAFORO.style.display = "none";
+    FORMULARIO.style.display = "block";
+    HEAD_FORMULARIO.style.backgroundColor = "red";
+    HEAD_CRONOMETRO.style.backgroundColor = "white";
+    HEAD_SEMAFORO.style.backgroundColor = "white";
+    
+}
+
+function verSemaforo() {
+    CRONOMETRO.style.display = "none";
+    SEMAFORO.style.display = "block";
+    FORMULARIO.style.display = "none";
+    HEAD_SEMAFORO.style.backgroundColor = "lightgrey";
+    HEAD_CRONOMETRO.style.backgroundColor = "white";
+    HEAD_FORMULARIO.style.backgroundColor = "white";
+
+}
+
+function verCronometro() {
+    CRONOMETRO.style.display = "block";
+    SEMAFORO.style.display = "none";
+    FORMULARIO.style.display = "none";
+    HEAD_CRONOMETRO.style.backgroundColor = "red";
+    HEAD_SEMAFORO.style.backgroundColor = "white";
+    HEAD_FORMULARIO.style.backgroundColor = "white";
+
+}
+
+INPUT_FORM.addEventListener("onclick", verFormulario)
+
+
+
+
+
+
+
 
