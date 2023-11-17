20f8340e20eb21cf12f5ddffc8abba86f111e90c
Jesus Lorenzo <jesus@iesclaradelrey.es>
2023-11-17 13:12:10 +0100

    comit 17/11/2023

--- Bloque 3/Programacion de servicios y procesos/Practica/Interbloqueo/src/Principal.java
+++ Bloque 3/Programacion de servicios y procesos/Practica/Interbloqueo/src/Principal.java
@@ -1,8 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Principal {
+
     public static void main(String[] args) {
+        Juguete arco=new Juguete("ARCO");
+        Juguete flecha=new Juguete("FLECHA");
 
+        // Creamos dos hilos con distintos órdenes de recursos para provocar el interbloqueo
+        Thread hilo1 = new Thread(new Infante("CAIN", arco, flecha), "Hilo 1 [CAIN]");
+        Thread hilo2 = new Thread(new Infante("ABEL", flecha, arco), "Hilo 2 [CAIN]");
 
+        hilo1.start();
+        hilo2.start();
     }
 }
