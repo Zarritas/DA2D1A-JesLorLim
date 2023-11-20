b532c00eb64c1d1373d64c157876344b2acdd57b
Zarras <jlorenzolimon@gmail.com>
2023-11-21 00:37:33 +0100

    fin

--- Bloque 4/Desarrollo web en entorno cliente/Practica/eventos/ej3/main.js
+++ Bloque 4/Desarrollo web en entorno cliente/Practica/eventos/ej3/main.js
@@ -70,6 +70,9 @@ function radioGeneral(evento) {
         case "formulario":
             verFormulario()
             break;
+const headForm = document.getElementById("div_form");
+const headCrono = document.getElementById("div_semaforo");
+const headSemaf = document.getElementById("div_cronometro");
 
         case "semaforo":
             verSemaforo()
@@ -283,3 +286,14 @@ document.addEventListener("click",(evento)=>{
         cronometroFuncional();
     }
 })
+
+if (document.querySelector('input[name="opciones"]')) {
+    document.querySelectorAll('input[name="opciones"]').forEach((elem) => {
+      elem.addEventListener("change", function(event) {
+        let item=event.target.value;
+        console.log(item);
+        elem.style.backgroundColor = "red";
+      });
+    });
+  }
+
